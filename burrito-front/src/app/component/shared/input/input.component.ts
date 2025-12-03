7465eac5493ff846d0ccfa9300ba16e858056699
Looki-fr <lemeilllouis@gmail.com>
2025-12-03 14:30:59 +0100

    input component

---
+++ burrito-front/src/app/component/shared/input/input.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [NgClass, FormsModule],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+})
+export class InputComponent {
+  /** 'primary' | 'outline' | 'ghost' */
+  @Input() variant: 'primary' | 'outline' | 'ghost' = 'primary';
+
+  /** full width */
+  @Input() fullWidth = false;
+
+  /** disabled */
+  @Input() disabled = false;
+
+  /** input type */
+  @Input() type:
+    | 'text'
+    | 'email'
+    | 'password'
+    | 'number'
+    | 'search'
+    | 'tel'
+    | 'url' = 'text';
+
+  /** placeholder */
+  @Input() placeholder = '';
+
+  /** name attribute */
+  @Input() name?: string;
+
+  /** ngModel value binding */
+  @Input() model: any;
+}
