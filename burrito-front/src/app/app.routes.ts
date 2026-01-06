828b92df1be6eeb39aff11ac52ec41c63c7917b0
Looki-fr <lemeilllouis@gmail.com>
2026-01-06 11:27:12 +0100

    privacy policy page

--- burrito-front/src/app/app.routes.ts
+++ burrito-front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PrivacyPolicyComponent } from './pages/privacy/privacy-policy.component';
 
 export const routes: Routes = [
   {
@@ -16,4 +17,9 @@ export const routes: Routes = [
     path: 'register',
     component: RegisterComponent
   },
+  {
+    path: 'legal/privacy',
+    component: PrivacyPolicyComponent
+  }
+
 ];
