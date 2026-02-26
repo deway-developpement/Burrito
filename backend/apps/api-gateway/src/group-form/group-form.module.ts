ad35db1b34d83ecab4ce4301e47b33d716627d1f
P4UL-M <paul.mairesse@free.fr>
2026-02-26 23:31:01 +0100

    fix(redis): add retry config for Nest Redis transport

--- backend/apps/api-gateway/src/group-form/group-form.module.ts
+++ backend/apps/api-gateway/src/group-form/group-form.module.ts
@@ -14,6 +14,8 @@ import { GroupFormsByFormLoader } from '../loaders/group-forms-by-form.loader';
           options: {
             port: parseInt(process.env.REDIS_PORT || '6379'),
             host: process.env.REDIS_HOST || 'localhost',
+            retryAttempts: parseInt(process.env.REDIS_RETRY_ATTEMPTS || '1000000'),
+            retryDelay: parseInt(process.env.REDIS_RETRY_DELAY_MS || '1000'),
           },
         }),
       },
